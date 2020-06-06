a8147a75e0b087e1dd716136f80f3a974e66c35c
MBI Berlin <x@mbi-berlin.de>
2020-06-06 11:09:08 +0200

    made pule trains asynchronous and independent of gatewidth

--- NI-USB_TDS/ni_usb_TDS.py
+++ NI-USB_TDS/ni_usb_TDS.py
@@ -12,26 +12,30 @@ from tango import AttrWriteType, DevState, DebugIt
 from tango.server import Device, attribute, command
 import time
 import ni_usb_6501 as ni
+from threading import Lock, Thread
 
 
 class NIUSB6501(Device):
     
     
-    port3 = attribute(fget='read_port3',fset='write_port3',name='Port_1.3',access=AttrWriteType.READ_WRITE,dtype=bool)
+    port3 = attribute(fget='read_port3',fset='write_port3',name='Port_1_3',access=AttrWriteType.READ_WRITE,dtype=bool)
     
-    port4 = attribute(fget='read_port4',fset='write_port4',name='Port_1.4',access=AttrWriteType.READ_WRITE,dtype=bool)
+    port4 = attribute(fget='read_port4',fset='write_port4',name='Port_1_4',access=AttrWriteType.READ_WRITE,dtype=bool)
     
-    port5 = attribute(fget='read_port5',fset='write_port5',name='Port_1.5',access=AttrWriteType.READ_WRITE,dtype=bool)
+    port5 = attribute(fget='read_port5',fset='write_port5',name='Port_1_5',access=AttrWriteType.READ_WRITE,dtype=bool)
     
-    port6 = attribute(fget='read_port6',fset='write_port6',name='Port_1.6',access=AttrWriteType.READ_WRITE,dtype=bool)
+    port6 = attribute(fget='read_port6',fset='write_port6',name='Port_1_6',access=AttrWriteType.READ_WRITE,dtype=bool)
     
-    port7 = attribute(fget='read_port7',fset='write_port7',name='Port_1.7',access=AttrWriteType.READ_WRITE,dtype=bool)
+    port7 = attribute(fget='read_port7',fset='write_port7',name='Port_1_7',access=AttrWriteType.READ_WRITE,dtype=bool)
     
     gatetime = attribute(fget='read_gatetime',fset='write_gatetime',name='Gatetime',access=AttrWriteType.READ_WRITE,dtype=float,min_value=0)
     
     act_port = attribute(fget='read_act_port',fset='write_act_port',name='ActivePort',access=AttrWriteType.READ_WRITE,dtype=int,min_warning=2, min_value=0,max_value=7)
     
     frequency = attribute(fget='read_frequency',fset='write_frequency',name='Frequency',access=AttrWriteType.READ_WRITE,dtype=float,min_value=1)
+    
+    npulses = attribute(name='npulses', access=AttrWriteType.READ_WRITE,
+                         dtype=int, min_value=1)
     
     def init_device(self):
         self.info_stream('Trying to establish connection')
@@ -49,6 +53,8 @@ class NIUSB6501(Device):
             self.__gatetime = 1
             self.__act_port = 3
             self.__freq = 1
+            self.__npulses = 1
+            self.__busy = False
             
         except:
             if not self.dev:
@@ -89,7 +95,12 @@ class NIUSB6501(Device):
     
     def write_port7(self,state):
         self.change_port(7,state)
-        
+    
+    def read_npulses(self):
+        return self.__npulses
+    
+    def write_npulses(self, val):
+        self.__npulses = val
         
     def read_gatetime(self):
         return self.__gatetime
@@ -134,34 +145,53 @@ class NIUSB6501(Device):
     @command()    
     def gate_timer(self):
         start = time.time()
+        self.__busy = True
         self.change_port(self.__act_port,True)
         self.info_stream('Port{} active'.format(self.__act_port))
         time.sleep(self.__gatetime)
         self.change_port(self.__act_port,False)
+        self.__busy = False
         self.info_stream('Port{} inactive'.format(self.__act_port))
         self.debug_stream('Actual duration of gate: '+ str(time.time()-start))
 
-    #The frequency of the pulsetrain is not correct (as one can see by the "hits" in debug-mode).
-    @DebugIt()
+    
     @command()
+    def train_async(self):
+        if not self.__busy:
+            t = Thread(target=self.pulsetrain)
+            t.daemon = True
+            t.start()
+    
+        
     def pulsetrain(self):
-        hits = 0
-
         bitmap_on = int(self.change_active(self.__act_port,True),2)
         bitmap_off = int(self.change_active(self.__act_port,False),2)
         self.debug_stream(str(bitmap_on))
         self.debug_stream(str(bitmap_off))
         
-        start = time.time()
-        while time.time() <= start+self.__gatetime:
-            hits += 1
-            self.dev.write_port(1,bitmap_on)
-            time.sleep(1/(self.__freq))
-            self.dev.write_port(1,bitmap_off)
-            time.sleep(1/(self.__freq))
-        act_dur = time.time()-start
-        self.debug_stream('Actual duration:'+str(act_dur))
-        self.debug_stream('Actual hits: '+str(hits))
+        # start = time.time()
+        # while time.time() <= start+self.__gatetime:
+        #     hits += 1
+        #     self.dev.write_port(1,bitmap_on)
+        #     time.sleep(1/(self.__freq))
+        #     self.dev.write_port(1,bitmap_off)
+        #     time.sleep(1/(self.__freq))
+        # act_dur = time.time()-start
+        self.set_state(DevState.RUNNING)
+        self.__busy = True
+        t0 = time.time()
+        for i in range(self.__npulses):
+            self.dev.write_port(1, bitmap_on)
+            time.sleep(1 / (2 * self.__freq))
+            self.dev.write_port(1, bitmap_off)
+            time.sleep(1 / (2 * self.__freq))
+        duration = time.time() - t0
+        print(f'elapsed: {duration:.2f}', file=self.log_debug)
+        self.set_state(DevState.ON)
+        self.__busy = False
+            
+        # self.debug_stream('Actual duration:'+str(act_dur))
+        # self.debug_stream('Actual hits: '+str(hits))
     
             
         
